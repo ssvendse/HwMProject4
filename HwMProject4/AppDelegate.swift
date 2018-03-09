e70f5a96226c4f11d5441c1d0849f402b2ac0809
Skyler Svendsen <skylersvendsen@gmail.com>
2018-03-08 19:33:23 -0600

    Initial Commit

---
+++ HwMProject4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HwMProject4
+//
+//  Created by Skyler Svendsen on 3/8/18.
+//  Copyright © 2018 Skyler Svendsen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HwMProject4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HwMProject4
+//
+//  Created by Skyler Svendsen on 3/8/18.
+//  Copyright © 2018 Skyler Svendsen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
